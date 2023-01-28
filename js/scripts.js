fb1afa9763dd113cb68cf7d85c4d3d0974935d06
KakiUX <120753819+KakiUX@users.noreply.github.com>
2023-01-28 12:18:36 +0000

    hamburger meun

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Life is more than just surviving")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
